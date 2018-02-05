b8365e3050ac2543f732f15eaf04f3ca47000728
alextomsk98 <shavlo.2016@mail.ru>
2018-02-05 17:32:24 +0700

    05.02.2018
    laba3

---
+++ lols/lols/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Basic Interitance *****\n");
+            Car myCar = new Car(80);
+            myCar.Speed = 50;
+            Console.WriteLine("My car is going{0} MPH,myCar.Speed");
+            Console.ReadLine();
+        }
+    }
+}
